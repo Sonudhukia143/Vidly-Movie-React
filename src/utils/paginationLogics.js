6e1af32facba27cf2afe8fcfb97ebda459c3dc33
Sonudhukia143 <jagdishdhukia770@gmail.com>
2024-10-05 23:45:24 +0530

    Updates

--- src/utils/paginationLogics.js
+++
@@ -1,7 +0,0 @@
-export default function changePage (page,PageLength,NumOfMovies,movieData) {
-    let arrOfNewItems = [];
-
-    arrOfNewItems = movieData.slice((page * PageLength) - PageLength , PageLength * page);
-
-    return arrOfNewItems;
-}
